445793df7afdf2e17595566638c572f60881203a
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-11-23 14:50:32 +0100

    add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Nitkleeners'
+    if page_title.empty?
+      base_title + ' | ' + 'Trusted For Neat Jobs'
+    else
+      base_title + ' | ' + page_title
+    end
+  end
 end
