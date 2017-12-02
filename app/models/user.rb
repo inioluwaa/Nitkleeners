08d3d97a49ca68b7d0a03ed6de32b03774007a7f
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-02 13:00:52 +0100

    finish successful signups, add flash  and add phone nunber uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
-  validates :phone_number, phone: true
+  validates :phone_number, phone: true, uniqueness: { case_sensitive: false }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 256 },
