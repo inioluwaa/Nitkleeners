e962adfd357afb6c1b9030af9db0274d2e535931
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-11 22:26:52 +0100

    find and authenticate a user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      render 'new'
+    end
+  end
+
+  def destroy; end
+end
