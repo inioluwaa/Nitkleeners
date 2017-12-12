205ec936d1a55211317bf7c5297e0897f5824743
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-12 17:59:51 +0100

    login upon signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,16 +17,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test 'valid signup information' do
     get signup_path
-    assert_no_difference 'User.count', 1 do
+    assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: 'Example User',
                                          email: 'user@example.com',
                                          password: 'password',
                                          password_confirmation: 'password',
-                                         phone_number: '09051234567'
-      } }
+                                         phone_number: '09051234567' } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.nil
+    assert is_logged_in?
+    assert_not flash.nil?
   end
 end
