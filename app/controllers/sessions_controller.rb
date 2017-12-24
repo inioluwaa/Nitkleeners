ae91b2c3ef8ac91a0444957c50c873a50577f9a0
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-24 09:20:49 +0100

    login with remembering and forgetting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email or password'
       render 'new'
     end
   end
