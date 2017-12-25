a228890346d8e69d7291481da98ea683692c338b
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-25 20:52:37 +0100

    testing remember me checkbox

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,4 +34,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
   end
+
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test 'login without remembering' do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
