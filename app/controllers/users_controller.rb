ca05086ae29e4be3d8f8aed1620bb98f83e02d73
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-12-30 14:23:31 +0100

    implement unsuccessful update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
