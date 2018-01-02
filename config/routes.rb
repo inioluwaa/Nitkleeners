d62ca4808465bb88462e170601b5bb9cdb74520c
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-02 23:09:41 +0100

    mailer template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
+
   get '/about', to: 'static_pages#about'
   get '/services', to: 'static_pages#services'
   get '/contact', to: 'static_pages#contact'
