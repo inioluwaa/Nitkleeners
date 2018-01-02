0a5e25caa24f9c2f14e7659dff166f68b0dda138
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-03 00:20:55 +0100

    email preview

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
