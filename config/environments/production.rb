4b38360e23312a20b6c5e5cd473a5239151005a1
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-06 18:34:01 +0100

    email in production and account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,19 @@ Rails.application.configure do
     }
   }
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'nitkleeners.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
