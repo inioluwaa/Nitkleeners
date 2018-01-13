eae1c8bb2cbc9fe343e8bbd18c45aef7f8064af4
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-13 20:51:48 +0100

    add products, validations and unit test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :products
   get 'password_resets/new'
 
   get 'password_resets/edit'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111202551) do
+ActiveRecord::Schema.define(version: 20180113070910) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
