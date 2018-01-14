e88195e963c072728046d6f9c20bc21e7c9c4eb8
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-14 18:30:07 +0100

    remove duplicate notice

--- db/migrate/20180113070910_create_products.rb
+++ db/migrate/20180113070910_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price, precision: 8
+      t.decimal :price
 
       t.timestamps
     end
