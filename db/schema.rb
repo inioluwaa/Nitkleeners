e4f2d0c2edf7f77b8af329a80532cbba789ab083
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-14 17:40:11 +0100

    use helper to format price

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20180113070910) do
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.decimal "price", precision: 8, scale: 2
+    t.decimal "price", precision: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
