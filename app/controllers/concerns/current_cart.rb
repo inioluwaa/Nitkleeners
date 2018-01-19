3b11bd346f8f8388cf506838b259eef142f2ffda
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-01-19 18:55:09 +0100

    fix bug

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,7 +3,7 @@ module CurrentCart
   private
 
   def set_cart
-    @cart = Cart.find(session[:id])
+    @cart = Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
     session[:cart_id] = @cart.id
