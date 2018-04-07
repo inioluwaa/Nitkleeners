1fa36619a637c873ee6cc0f702ee3167f1bf57a1
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2018-04-07 23:49:43 +0100

    add stripe for payment

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   get '/store', to: 'store#index'
 
   resources :users
+  resources :charges
   resources :account_activations, only: [:edit]
   resources :password_resets, only: %i[new create edit update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
