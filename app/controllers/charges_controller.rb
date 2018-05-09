780be2145b229cf7c52d49971ab2986a75b49637
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-05-09 10:54:20 +0100

    change layout of cart

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -14,12 +14,12 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       customer: customer.id,
       amount: @amount,
-      description: 'Rails Stripe customer',
+      description: 'Nitkleeners Customer',
       currency: 'usd'
     )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_charge_path
+    redirect_to store_path
   end
 end
