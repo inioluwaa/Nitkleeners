fe21524b91500c184f3ae0347b7b740d76555d74
Ini Akinleye <inioluwaakinleye@gmail.com>
2018-06-23 21:29:12 +0100

    remove contactmailer

--- test/mailers/previews/contact_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
-class ContactMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/contact_mailer/contact_mailer
-  def contact_mailer
-    ContactMailer.contact_mailer
-  end
-
-end
